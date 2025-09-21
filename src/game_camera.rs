a5ec4ddb705cecea2b5d1cb89ceded56ac424a70
exepta <genshin.exepta@outlook.com>
2025-09-21 06:31:52 +0200

    new(#2): build simple test room for late test player movement.

--- src/game_camera.rs
+++ src/game_camera.rs
@@ -5,8 +5,8 @@ use game_core::states::AppState;
 pub struct GameCameraPlugin;
 
 impl Plugin for GameCameraPlugin {
-    
-    #[coverage(off)]   
+
+    #[coverage(off)]
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(AppState::Preload), setup_ui_camera);
         app.add_systems(OnEnter(AppState::PostLoad), setup_game_camera);
@@ -22,11 +22,11 @@ fn setup_game_camera(mut commands: Commands) {
             ..default()
         },
         Msaa::Sample4,
-        Projection::Orthographic(OrthographicProjection {
-            scaling_mode: ScalingMode::WindowSize,
-            scale: 1.0,
-            ..OrthographicProjection::default_3d()
-        }),
+        Transform {
+            translation: Vec3::new(5.0, 35.0, 55.0),
+            rotation: Quat::from_rotation_x(-35.0_f32.to_radians()),
+            ..Default::default()
+        }
     ));
 }
 
@@ -36,6 +36,7 @@ fn setup_ui_camera(mut commands: Commands) {
         Camera2d::default(),
         Camera {
             order: 1,
+            clear_color: ClearColorConfig::None,
             ..default()
         },
         Msaa::Sample4,
